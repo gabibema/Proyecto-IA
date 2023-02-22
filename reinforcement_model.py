f58a7320d868c72b42d8fe196bb27dca058a00f7
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2023-02-22 16:05:12 -0300

    qtrainer init

--- reinforcement_model.py
+++ reinforcement_model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 
 class Linear_QNet(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
@@ -12,3 +13,13 @@ class Linear_QNet(nn.Module):
         x = F.relu(self.linear1(x))
         x = self.linear2(x)
         return x
+    
+
+class QTrainer:
+     def __init__(self,model,lr,gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimer = optim.Adam(model.parameters(),lr = self.lr)    
+        self.criterion = nn.MSELoss()
+
